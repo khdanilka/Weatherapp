6db1622a562186688df1728cbde276889dc4c813
khdanilka <daniil.khalupskiy@gmail.com>
2017-11-06 01:55:25 +0500

    lesson 4 hw finished

--- app/src/main/java/weather/khdanapp/com/weatherapp/MainActivity.java
+++ app/src/main/java/weather/khdanapp/com/weatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package weather.khdanapp.com.weatherapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -24,7 +25,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         city_selection = (Spinner) findViewById(R.id.spin);
         int city_n = DataClass.readData(this);
-        if (city_n > 0) city_selection.setSelection(city_n);
+        if (savedInstanceState!= null) {
+            int k = savedInstanceState.getInt(CITY_INSTANCE);
+            city_selection.setSelection(k);
+        }
+        else if (city_n > 0) city_selection.setSelection(city_n);
 
         Button find_b = (Button) findViewById(R.id.find);
         find_b.setOnClickListener(MainActivity.this);
@@ -38,8 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if   (view .getId()  ==  R.id.find)   {
                 int  city_pos  = city_selection.getSelectedItemPosition();
                 String cityWeather = DataClass.getWeatherByCityId(this,city_pos);
-                DataClass.writeToPref(this,city_pos,cityWeather);
-
+//                DataClass.writeToPref(this,city_pos,cityWeather);
                 String mes = city_selection.getSelectedItem() + ": " + cityWeather;
                 Intent n = new Intent(this,ResultActivity.class);
                 n.putExtra(ResultActivity.EXTRA_MESSAGE, mes);
@@ -59,5 +63,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+
+        super.onDestroy();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume ");
+    }
+
+    private static final String CITY_INSTANCE = "city_instance";
+
+    protected void onSaveInstanceState(Bundle outState) {
+        int  city_pos  = city_selection.getSelectedItemPosition();
+        outState.putInt(CITY_INSTANCE, city_pos);
+        super.onSaveInstanceState(outState);
+        Log.d(LOG_TAG, "onSaveInstanceState");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    protected void onStop() {
+
+        int  city_pos  = city_selection.getSelectedItemPosition();
+        String cityWeather = DataClass.getWeatherByCityId(this,city_pos);
+        DataClass.writeToPref(this,city_pos);
+
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+
 }
 
