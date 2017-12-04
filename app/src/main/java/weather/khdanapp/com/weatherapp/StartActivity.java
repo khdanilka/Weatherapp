16feb3251dc57f69f5922b6d0cc7a65c8a06e6b6
khdanilka <daniil.khalupskiy@gmail.com>
2017-12-04 16:45:39 +0500

    part 2: lesson 2 hw nav drawer item clicked added

--- app/src/main/java/weather/khdanapp/com/weatherapp/StartActivity.java
+++ app/src/main/java/weather/khdanapp/com/weatherapp/StartActivity.java
@@ -1,12 +1,16 @@
 package weather.khdanapp.com.weatherapp;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class StartActivity extends AppCompatActivity implements FragmentOneListener {
+public class StartActivity extends AppCompatActivity implements FragmentOneListener, NavigationView.OnNavigationItemSelectedListener {
 
     FragmentOne detailFragment;
 
@@ -23,8 +27,10 @@ public class StartActivity extends AppCompatActivity implements FragmentOneListe
 
         transaction.commit();
 
-    }
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
 
+    }
 
 
     @Override
@@ -42,5 +48,35 @@ public class StartActivity extends AppCompatActivity implements FragmentOneListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_start);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_weather) {
+            // Handle the camera action
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_feedback) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_start);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
