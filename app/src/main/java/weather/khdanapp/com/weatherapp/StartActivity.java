e32236830fcb69a654e370469223a4a8b8d8a542
khdanilka <daniil.khalupskiy@gmail.com>
2017-12-05 01:47:39 +0500

    part 2: lesson 2 nav drawer completely added

--- app/src/main/java/weather/khdanapp/com/weatherapp/StartActivity.java
+++ app/src/main/java/weather/khdanapp/com/weatherapp/StartActivity.java
@@ -5,8 +5,10 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,15 +22,23 @@ public class StartActivity extends AppCompatActivity implements FragmentOneListe
         setContentView(R.layout.activity_start);
         detailFragment = new FragmentOne();
 
-        //detailFragment.setWeatherId(categoryId);
-        //detailFragment.setMarkWhatNeed(checkBoxValue);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.frame_segment, detailFragment);
-
         transaction.commit();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_start);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_start);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
     }
 
